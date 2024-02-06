a2cb3b58dd64724d71429112501fd0d857077820
juandavidmunozdev <159089475+juandavidmunozdev@users.noreply.github.com>
2024-02-05 20:45:54 -0500

    Add files via upload

---
+++ reto_dia_1.py
@@ -0,0 +1,14 @@
+# Para este primer reto de la semana, tu objetivo será poder crear un programa en Python el cual permita registrar a un usuario en el sistema.
+# Para ello el programa deberá pedir a nuestro usuario final ingrese su siguiente información.
+
+# Solicitar información al usuario
+nombre = input("Ingrese su(s) nombre(s): ")
+apellidos = input("Ingrese su(s) apellidos: ")
+telefono = input("Ingrese su número de teléfono: ")
+correo_electronico = input("Ingrese su correo electrónico: ")
+
+# Mensaje de bienvenida
+print("\nHola " + nombre + " " + apellidos + ", en breve recibirás un correo a " + correo_electronico +".")
+
+
+
